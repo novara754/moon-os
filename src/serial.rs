d42234ef050ea387deb3c4e0efece697f57de52a
vzwGrey <vzwgrey@gmail.com>
2022-05-22 11:25:59 +0200

    refactor: use uart_16550 library

--- src/serial.rs
+++ src/serial.rs
@@ -1,22 +1,11 @@
 use lazy_static::lazy_static;
 use spin::Mutex;
+use uart_16550::SerialPort;
+
+pub const COM1_PORT: u16 = 0x3F8;
 
 lazy_static! {
-    static ref COM1: Mutex<Com1> = Mutex::new(Com1);
-}
-
-struct Com1;
-
-impl core::fmt::Write for Com1 {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        for &b in s.as_bytes() {
-            unsafe {
-                let port = 0x3F8; // COM1
-                core::arch::asm!("out dx, al", in("dx") port, in("al") b, options(nomem, nostack, preserves_flags));
-            }
-        }
-        Ok(())
-    }
+    static ref COM1: Mutex<SerialPort> = Mutex::new(unsafe { SerialPort::new(COM1_PORT) });
 }
 
 #[macro_export]
